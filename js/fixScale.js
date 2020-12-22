b076250f1c75201efb0078943a6eedab8af6b092
Jon Wyble <69123330+jwyble@users.noreply.github.com>
2020-12-21 20:03:29 -0500

    Add files via upload

---
+++ js/fixScale.js
@@ -0,0 +1,26 @@
+/*
+  JavaScript file for scale fixing in the LiteLeaf template
+  Copyright (C) Jon Wyble
+  Available under the Creative Commons Attribution 4.0 International license
+*/
+
+var i;
+var metas = document.getElementsByTagName('meta');
+
+function startGesture() {
+	for (i = 0; i < metas.length; i++) {
+		if (metas[i].name == 'viewport') {
+			metas[i].content = "width=device-width, maximum-scale=1.25, minimum-scale=1.5";
+		}
+	}
+}
+
+if (navigator.userAgent.match(/iPhone/i)) {
+	for (i = 0; i < metas.length; i++) {
+		if (metas[i].name == 'viewport') {
+			metas[i].content = "width=device-width, maximum-scale=1.0, minimum-scale=1.0";
+		}
+	}
+
+	document.addEventListener("gestureStart", startGesture, false);
+}
